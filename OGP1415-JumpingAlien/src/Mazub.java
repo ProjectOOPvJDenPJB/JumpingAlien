e30eb621793e665ca8184700e653fe9f0b25fe80
JorenDhont <rjvqBEPdgK4%>
2015-02-23 23:07:03 +0100

    Revert "Test"
    This reverts commit 8cd18f72528ce6dec760ec2573fd999f90d81b0c.

--- OGP1415-JumpingAlien/src/Mazub.java
+++ OGP1415-JumpingAlien/src/Mazub.java
@@ -13,7 +13,6 @@ import be.kuleuven.cs.som.annotate.*;
 /**
  * 
  * NAKIJKEN: Op welke manier elke methode moet geïmplementeerd worden.
- * Dit is belangrijk
  * 
  * comment naar joren: 
  * moeten we acceleration, vmax, en vinitial wel aan ons object zelf toevoegen? En nie gewoon een waarde binnen de code?
