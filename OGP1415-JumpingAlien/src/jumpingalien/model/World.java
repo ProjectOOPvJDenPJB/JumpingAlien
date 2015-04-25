24a7d5318d414e44c0890a755d5a23ba172aa626
JorenDhont <joren.dhont@gmail.com>
2015-04-25 16:04:21 +0200

    Commentaren world af (buiten 1 functie)

--- OGP1415-JumpingAlien/src/jumpingalien/model/World.java
+++ OGP1415-JumpingAlien/src/jumpingalien/model/World.java
@@ -2,10 +2,8 @@ package jumpingalien.model;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import jumpingalien.util.Util;
@@ -145,7 +143,7 @@ public class World {
 	 * 			The Y coordinate of the given position.
 	 * @post	if (pixelX > getPixelWidth())
 	 * 				then (position[0] = getPixelWidth())
-	 * @post	if (pixelY > getPixelHeigh())
+	 * @post	if (pixelY > getPixelHeight())
 	 * 				then (position[1] = getPixelHeigth())
 	 * @return	An array of integers where position[0] is the x_T position
 	 * 			and position [1] is the x_Y position
@@ -227,18 +225,16 @@ public class World {
 		return this.tileTypes[position[0]][position[1]];
 	}
 	
+	/**
+	 * Return whether the tile of the given position is passable.
+	 * @param 	leftX
+	 * @param 	bottomY
+	 * @return	...
+	 * 			| return (tileTypes[tile_X][tile_Y] != 1)
+	 */
 	public boolean isPassable(int leftX,int bottomY){
 		int[] position = getTilePosition(leftX, bottomY);
-			if (this.tileTypes[position[0]][position[1]] == 1) {
-				return false;
-//			if ((bottomY % getTileSize() == getTileSize()-1) &&
-//					(isPassable(leftX, bottomY+1)))
-//				return true;
-//			else
-//				return false;
-		}
-		else
-			return true;
+		return (this.tileTypes[position[0]][position[1]] != 1);
 	}
 	
 	/**
@@ -253,7 +249,9 @@ public class World {
 	 * @param 	rightX
 	 * @param 	topY
 	 * @return	...
-	 * 			| //TODO like wtf moete wij hier schrijve
+	 * 			| return tilesInt[][]
+	 * 			|	where (tilesInt[i][0] == tile_X)
+	 * 			|	and (tilesInt[i][1] == tile_Y)
 	 */
 	public int[][] getOccupiedTiles(int leftX, int bottomY, int rightX, int topY) {
 		List<int[]> tiles = new ArrayList<int[]>();
@@ -293,6 +291,15 @@ public class World {
 			return length / tileLength;
 	}
 	
+	/**
+	 * Checks whether the given window with given width and height is a valid
+	 * 	visible window.
+	 * @param 	windowWidth
+	 * @param 	windowHeight
+	 * @return	...
+	 * 			| return ((winwowWidth <= getPixelWidth()) && (windowHeight <= getPixelHeight())
+	 * 			|	&& (windowWidth => 0) && (windowHeight >= 0))
+	 */
 	public boolean isValidVisibleWindow(int windowWidth, int windowHeight) {
 		return ((Util.fuzzyLessThanOrEqualTo(windowWidth, getPixelWidth())) && (Util.fuzzyLessThanOrEqualTo(windowHeight, getPixelHeight()))
 				&& (Util.fuzzyGreaterThanOrEqualTo(windowWidth, 0)) && (Util.fuzzyGreaterThanOrEqualTo(windowHeight, 0)));
@@ -334,8 +341,30 @@ public class World {
 	 */
 	private Position visibleWindowPosition = new Position(0,0,this);
 	
-
-	public void setWindowPosition(double leftX, double bottomY) throws IllegalXPositionException, IllegalYPositionException {
+	/**
+	 * Set the new position of the visible window to the given position.
+	 * @param 	leftX
+	 * @param 	bottomY
+	 * @post	...
+	 * 			| if (leftX < 0)
+	 * 			|	then new.getVisibleWindowPosition().getXPosition() == 0
+	 * @post	...
+	 * 			| if (leftX > (getPixelWidth() - getVisibleWindowWidth())
+	 * 			|	then new.getVisibleWindowPosition().getXPosition() 
+	 * 			|		== getPixelWidth() - getVisibleWindowWidth()
+	 * @post	...
+	 * 			| if (bottomY < 0)
+	 * 			|	then new.getVisibleWindowPosition().getYPosition() == 0
+	 * @post	...
+	 * 			| if (bottomY > (getPixelHeight() - getVisibleWindowHeight())
+	 * 			|	then new.getVisibleWindowPosition().getYPosition() 
+	 * 			|		== getPixelHeight() - getVisibleWindowHeight()
+	 * @post	...
+	 * 			| if (leftX > 0) && (leftX < (getPixelWidth() - getVisibleWindowWidth())
+	 * 			|		&& (BottomY > 0) && (bottomY < (getPixelHeight() - getVisibleWindowHeight())
+	 * 			| 	then new.getVisibleWindowPosition().getPosition() == [leftX,bottom]
+	 */
+	public void setWindowPosition(double leftX, double bottomY) {
 		if (leftX < 0)
 			leftX = 0;
 		else if (leftX > (getPixelWidth() - getVisibleWindowWidth()))
@@ -347,6 +376,29 @@ public class World {
 		visibleWindowPosition = new Position(leftX, bottomY, this);
 	}
 	
+	/**
+	 * Updates the visible window position based on the position of Mazub.
+	 * @post	...
+	 * 			| if (getMazub().getXPosition() < getVisibleWindowPosition().getXPosition() + 200)
+	 * 			|	then new.getVisibleWindowPosition.getXPosition == 
+	 * 			|		getMazub().getXPosition() - 200
+	 * @post	...
+	 * 			| if (getMazub().getXPosition() > (getVisibleWindowPosition().getXPosition()
+	 * 			|		+ getVisibleWindowWidth() - 200))
+	 * 			|	then new.getVisibleWindowPosition.getXPosition ==
+	 * 			|		getMazub().getXPosition() - (getVisibleWindowPosition().getXPosition()
+	 * 			|		+ getVisibleWindowWidth() - 200)
+	 * @post	...
+	 * 			| if (getMazub().getYposition() < getVisibleWindowPosition().getYPosition() + 200)
+	 * 			|	then new.getVisibleWindowPosition.getYPosition == 
+	 * 			|		getMazub().getYPosition() - 200
+	 * @post	...
+	 * 			| if (getMazub().getYPosition() > (getVisibleWindowPosition().getYPosition()
+	 * 			|		+ getVisibleWindowHeight() - 200))
+	 * 			|	then new.getVisibleWindowPosition.getYPosition ==
+	 * 			|		getMazub().getYPosition() - (getVisibleWindowPosition().getYPosition()
+	 * 			|		+ getVisibleWindowHeight() - 200)
+	 */
 	public void updateWindowPosition() {
 		Position mazubPos = getMazub().getPosition();
 		Position windowPos = getVisibleWindowPosition();
@@ -367,6 +419,17 @@ public class World {
 			setWindowPosition(windowPos.getXPosition(), mazubPos.getYPosition() + 200);
 	}
 	
+	/**
+	 * Return the positions of the visible window.
+	 * @return	...
+	 * 			| pos[]
+	 * 			| where
+	 * 			|	pos[0] == getVisibleWindowPosition().getXPosition()
+	 * 			|	pos[1] == getVisibleWindowPosition().getYPosition()
+	 * 			|	pos[2] == getVisibleWindowPosition().getXPosition() + getVisibleWindowWidth()
+	 * 			|	pos[3] == getVisibleWindowPosition().getYPosition() + getVisibleWindowHeight()
+	 * 			
+	 */
 	public int[] getVisibleWindowPositionArray() {
 		int[] pos = new int[] {(int) getVisibleWindowPosition().getXPosition(),
 			(int) getVisibleWindowPosition().getYPosition(),
@@ -374,47 +437,113 @@ public class World {
 			(int) getVisibleWindowPosition().getYPosition()+getVisibleWindowHeight()
 		};
 		return pos;
-	}	
+	}
 	
+	/**
+	 * Return the amount of objects currently in this world.
+	 */
+	public int getAmountOfObjects() {
+		return getSlimes().size() + getPlants().size() + getSharks().size() + 1;
+	}
+	
+	/**
+	 * Checks whether the given world has a valid amount of objects.
+	 * @param 	world
+	 * @return	...
+	 * 			| return (world.getAmountOfObjects() < 100) &&
+	 * 			|	(world.getAmountOfObjects() >=0)
+	 */
 	public static boolean hasValidAmountOfObjects(World world) {
-		int size = world.slimes.size() + world.plants.size() + world.sharks.size();
+		int size = world.getAmountOfObjects();
 		return (size < 100) && (size >= 0);
 	}
 	
+	/**
+	 * Return the plants currently in this world in the form of a collection.
+	 */
 	public Collection<Plant> getPlants(){
 		Collection<Plant> elements = plants;
 		return new ArrayList<Plant>(elements);
 	}
 	
+	/**
+	 * Checks whether this world contains the given plant.
+	 * @param 	plant
+	 * @return	...
+	 * 			| return plants.contains(plant)
+	 */
     public boolean hasAsPlant(Plant plant) {
        return plants.contains(plant);
     }
 	
+    /**
+     * Variable registering the plants currently in this world.
+     */
 	private final Set<Plant> plants = new HashSet<Plant>();
 	
+	/**
+	 * Adds the given plant to this world.
+	 * @param 	plant
+	 * @pre		...
+	 * 			| assert plant.canHaveAsWorld(this)
+	 * @post	...
+	 * 			| new.hasAsPlant(plant) == true
+	 * @post	...
+	 * 			| plant.getWorld() == this
+	 */
 	public void addPlant(Plant plant) {
 		assert plant.canHaveAsWorld(this);
 		plants.add(plant);
 		plant.setWorld(this);
 	}
 	
+	/**
+	 * Removes the given plant from this world.
+	 * @param 	plant
+	 * @pre		...
+	 * 			| assert (hasAsPlant(plant) && plant.hasAsWorld(this))
+	 * @post	...
+	 * 			| new.hasAsPlant(plant) == false
+	 */
 	public void removePlant(Plant plant) {
 		assert this.hasAsPlant(plant) && (plant.hasAsWorld(this));
 		plants.remove(plant);
 	}
 	
+	/**
+	 * Return the slimes currently in this world in the form of a collection.
+	 */
 	public Collection<Slime> getSlimes(){
 		Collection<Slime> elements = slimes;
 		return new ArrayList<Slime>(elements);
 	}
 	
+	/**
+	 * Checks whether this world contains the given slime.
+	 * @param 	slime
+	 * @return	...
+	 * 			| return slimes.contains(slime)
+	 */
 	public boolean hasAsSlime(Slime slime) {
         return slimes.contains(slime);
 
     }
 	
+	/**
+     * Variable registering the slimes currently in this world.
+     */
 	private final Set<Slime> slimes = new HashSet<Slime>();
 	
+	/**
+	 * Adds the given slime to this world.
+	 * @param 	slime
+	 * @pre		...
+	 * 			| assert slime.canHaveAsWorld(this)
+	 * @post	...
+	 * 			| new.hasAsSlime(slime) == true
+	 * @post	...
+	 * 			| slime.getWorld() == this
+	 */
 	public void addSlime(Slime slime) {
 		assert slime.canHaveAsWorld(this);
 		if (!Interaction.interactWithMovementBlockingCreature(slime,this)){
@@ -423,53 +552,143 @@ public class World {
 		}
 	}
 	
+	/**
+	 * Removes the given slime from this world.
+	 * @param 	slime
+	 * @pre		...
+	 * 			| assert (hasAsSlime(slime) && slime.hasAsWorld(this))
+	 * @post	...
+	 * 			| new.hasAsSlime(slime) == false
+	 */
 	public void removeSlime(Slime slime) {
 		assert this.hasAsSlime(slime) && (slime.hasAsWorld(this));
 		slimes.remove(slime);
 	}
 	
+	/**
+	 * Return the sharks currently in this world in the form of a collection.
+	 */
 	public Collection<Shark> getSharks(){
 		Collection<Shark> elements = sharks;
 		return new ArrayList<Shark>(elements);
 	}
 	
+	/**
+	 * Checks whether this world contains the given shark.
+	 * @param 	shark
+	 * @return	...
+	 * 			| return sharks.contains(shark)
+	 */
 	public boolean hasAsShark(Shark shark) {
           return sharks.contains(shark);
     }
 	
+	/**
+     * Variable registering the sharks currently in this world.
+     */
 	private final Set<Shark> sharks = new HashSet<Shark>();
 	
+	/**
+	 * Adds the given shark to this world.
+	 * @param 	shark
+	 * @pre		...
+	 * 			| assert shark.canHaveAsWorld(this)
+	 * @post	...
+	 * 			| new.hasAsShark(shark) == true
+	 * @post	...
+	 * 			| shark.getWorld() == this
+	 */
 	public void addShark(Shark shark) {
 		assert shark.canHaveAsWorld(this);
 		sharks.add(shark);
 		shark.setWorld(this);
 	}
 	
+	/**
+	 * Removes the given shark from this world.
+	 * @param 	shark
+	 * @pre		...
+	 * 			| assert (hasAsShark(shark) && shark.hasAsWorld(this))
+	 * @post	...
+	 * 			| new.hasAsShark(shark) == false
+	 */
 	public void removeShark(Shark shark) {
 		assert this.hasAsShark(shark) && (shark.hasAsWorld(this));
 		sharks.remove(shark);
 	}
 	
-	public boolean canHaveAsObject(Object object) {
-		assert hasValidAmountOfObjects(this);
-		assert (object instanceof LivingCreatures);
-		if (isTerminated())
-			return object == null;
-		return (object != null) && (((LivingCreatures) object).canHaveAsWorld(this));
+	/**
+	 * Check whether the given object is of a valid type for this world.
+	 * @param 	object
+	 * @return	If the given object is of a valid type for this world,
+	 * 			true is returned. Otherwise false is returned.
+	 */
+	public boolean isValidObject(Object object) {
+		return (object instanceof LivingCreatures);
 	}
 	
+	/**
+	 * Checks whether the given object can be in this world.
+	 * @param 	object
+	 * @pre		...
+	 * 			| isValidObject(object)
+	 * @return 	...
+	 *			| if (this.isTerminated())
+	 *			|	return object == null;
+	 *			| else
+	 * 			|	 return ((object != null) && 
+	 * 			|		((object.canHaveAsWorld(this) &&
+	 * 			|		hasValidAmountOfObjects(this)
+	 */
+	public boolean canHaveAsObject(Object object) {
+		assert isValidObject(object);
+		if (isTerminated())
+			return object == null;
+		else
+			return (object != null) && (((LivingCreatures) object).canHaveAsWorld(this)) 
+					&& (hasValidAmountOfObjects(this));
+	}
+	
+	/**
+	 * Return the Mazub currently in this world.
+	 */
 	public Mazub getMazub() {
 		return this.mazub;
 	}
 	
+	/**
+	 * Variable registering the Mazub in this world.
+	 * 	The default value is null.
+	 */
 	private Mazub mazub = null;
 		
+	/**
+	 * Sets the given Mazub in this world.
+	 * @param 	alien
+	 * @pre		...
+	 * 			| assert canHaveAsMazub(alien)
+	 * @post	...
+	 * 			| new.getMazub() == alien
+	 * @post	...
+	 * 			| alien.getWorld() == this
+	 */
 	public void setMazub(Mazub alien) {
 		assert canHaveAsMazub(alien);
 		mazub = alien;
 		mazub.setWorld(this);
 	}
 	
+	/**
+	 * Checks whether this world can have the given Mazub as its Mazub.
+	 * @param 	alien
+	 * @return 	...
+	 *			| if (this.isTerminated())
+	 *			|	return alien == null;
+	 *			| else
+	 * 			|	 return ((alien != null) && 
+	 * 			|		((alien.canHaveAsWorld(this) &&
+	 * 			|		(getMazub() == null)
+	 */
 	public boolean canHaveAsMazub(Mazub alien) {
 		if (isTerminated())
 			return alien == null;
@@ -484,6 +703,9 @@ public class World {
 		return this.getState() == State.TERMINATED;
 	}
 	
+	/**
+	 * In-class enumeration of the different States of a world.
+	 */
 	private static enum State {
 		INITIALISED, STARTED, TERMINATED;
 	}
@@ -518,6 +740,14 @@ public class World {
 		this.state = state;
 	}
 	
+	/**
+	 * Advances the time with a given timeInterval and changes every time-related attribute of this World
+	 * 	accordingly.
+	 * @param 	dt
+	 * @post	AdvanceTime of the Mazub of this world was executed.
+	 * @post	The visible window position was updated.
+	 * @post	For every living creature in this world, advanceTime was executed.
+	 */
 	public void advanceTime(double dt) {
 		//System.out.println("MAZUB");
 		mazub.advanceTime(dt);
@@ -533,7 +763,11 @@ public class World {
 			shark.advanceTime(dt);
 		}
 	}
-
+	
+	/**
+	 * Terminates this world.
+	 * TODO
+	 */
 	public void terminate() {
 		if (! isTerminated()) {
 			for (Plant plant : getPlants()) {
@@ -551,6 +785,11 @@ public class World {
 		}
 	}
 	
+	/**
+	 * Checks whether the player (Mazub) has won the game.
+	 * @return	If Mazub occupies the target tile of this World, true is returned
+	 * 			otherwise false is returned.
+	 */
 	public boolean hasWonGame() {
 		int[] mazubPos = getMazub().getPosition().getPosition();
 		int[] mazubTile = getTilePosition(mazubPos[0], mazubPos[1]);
@@ -560,6 +799,7 @@ public class World {
 			return false;
 	}
 	
+	//Dit moet eigenlijk nog met State.INITIALISED gedaan worden
 	public void setGameStarted(boolean flag){
 		this.gameStarted = flag;
 	}
