7f9270995bdef074dda0abf7e62e4279d72cdff3
JorenDhont <joren.dhont@gmail.com>
2015-05-09 15:42:37 +0200

    da gaat ier moeilijk worden

--- OGP1415-JumpingAlien/src/jumpingalien/model/Buzam.java
+++ OGP1415-JumpingAlien/src/jumpingalien/model/Buzam.java
@@ -4,8 +4,15 @@ import jumpingalien.util.Sprite;
 
 public class Buzam extends Mazub {
 
-	public Buzam(int positionLeftX, int positionBottomY, Sprite[] spriteArray) {
-		super(positionLeftX, positionBottomY, spriteArray);
+	public Buzam(int positionLeftX, int positionBottomY, Sprite[] sprites,
+			double maximumHorizontalVelocity, double initialHorizontalVelocity,
+			World world, int hitpoints) {
+		super(positionLeftX, positionBottomY, sprites, maximumHorizontalVelocity,
+				initialHorizontalVelocity, world, hitpoints);
 	}
-
+	
+	public Buzam(int positionLeftX, int positionBottomY, Sprite[] sprites) {
+		this(positionLeftX,positionBottomY,sprites,3,1,null,500);
+	}
+	
 }
